1b64e89e1a4fd5eb609c1f2fa719cf3113faa56c
Alejandro Avendaño <avendanio2000@hotmail.com>
2022-06-03 16:45:27 -0300

    Agregar archivos de proyecto.

---
+++ JSUnitTestingDemo/Scripts/StringCalculator.js
@@ -0,0 +1,20 @@
+﻿(function (window) {
+    function add(stringNumbers) {
+        if (stringNumbers == '')
+            return 0;
+        else
+            return -1;
+    };
+
+    function long(stringP) {
+        return stringP.length;
+    };
+
+    function sum(n1, n2) {
+        return n1 + n2;
+    };
+
+    window.add = add;
+    window.long = long;
+    window.sum = sum;
+})(window);
---
+++ JSUnitTestingDemo/Specs/StringCalculatorTests.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="..\Scripts/jasmine/jasmine.js" />
+/// <reference path="..\Scripts/StringCalculator.js" />
+describe('String Calculator', function () {
+    it("should return zero when an empty string is passed", function () {
+        expect(add('')).toEqual(0);
+    });
+
+    it("should return string long", function () {
+        expect(long('Angular7')).toEqual(8);
+    });
+
+    it("should return sum numbers", function () {
+        expect(sum(8, 4)).toEqual(48);
+    });
+});
